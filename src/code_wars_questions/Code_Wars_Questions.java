e2d84c73dc25a479fd9d1e55ee0892273c1da06e
Aidan Holland <aidanjames11@hotmail.com>
2018-08-28 16:27:02 +1000

    First Question Entered with Test

---
+++ src/code_wars_questions/Code_Wars_Questions.java
@@ -0,0 +1,14 @@
+
+package code_wars_questions;
+
+
+public class Code_Wars_Questions {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
---
+++ src/code_wars_questions/Solution1.java
@@ -0,0 +1,10 @@
+package code_wars_questions;
+
+
+public class Solution1 
+{
+    public static String solve(String s, int k) 
+    {
+        return ""; // Do your magic!
+    }
+}
